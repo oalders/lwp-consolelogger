7db7176ec8211cc0fd681edd851870d72d939b4c
Olaf Alders <olaf@wundersolutions.com>
2026-05-12 17:28:17 +0000

    increment $VERSION after 1.000002 release

--- lib/LWP/ConsoleLogger/Everywhere.pm
+++ lib/LWP/ConsoleLogger/Everywhere.pm
@@ -2,7 +2,7 @@ package LWP::ConsoleLogger::Everywhere;
 use strict;
 use warnings;
 
-our $VERSION = '1.000002';
+our $VERSION = '1.000003';
 
 use Class::Method::Modifiers ();
 use LWP::ConsoleLogger::Easy qw( debug_ua );
